684f046dced1297e66ee773dff04118db1f1b71c
Jeremy Junior <jrjn71780@gmail.com>
2026-05-07 10:15:27 +0300

    Setup Flask core application structure

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+from config import Config
+
+from extensions import (
+    db,
+    migrate,
+    jwt,
+    socketio,
+    cors
+)
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    socketio.init_app(app)
+    cors.init_app(app)
+
+    # Test route
+    @app.route("/")
+    def home():
+        return {
+            "message": "Kazi Mikononi backend running"
+        }
+
+    return app
---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    # SQLite database (local file)
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///kazi_mikononi.db")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
