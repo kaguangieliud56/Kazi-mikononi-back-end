6d8e49220624bf3fe43ec5878a29460d6a30471a
Jeremy Junior <jrjn71780@gmail.com>
2026-05-18 10:26:29 +0300

    mailtrap

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
@@ -15,15 +13,15 @@ class Config:
     # -------------------------
     # MAIL CONFIG
     # -------------------------
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "localhost")
     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
 
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
+    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False").lower() == "true"
 
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", MAIL_USERNAME)
 
-    MAIL_TIMEOUT = 10
+    MAIL_TIMEOUT = int(os.getenv("MAIL_TIMEOUT", 10))
