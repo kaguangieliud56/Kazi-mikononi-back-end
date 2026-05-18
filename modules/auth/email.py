b159eddf01f391fa32cc79c85075f9e68858262b
Jeremy Junior <jrjn71780@gmail.com>
2026-05-18 09:45:45 +0300

    Add Gmail timeout handling

--- modules/auth/email.py
+++ modules/auth/email.py
@@ -4,8 +4,36 @@ from flask import current_app
 
 
 def send_verification_email(user_email, token):
-    print("EMAIL SKIPPED (production safe mode)")
 
-    link = f"https://kazi-mikononi-back-end.onrender.com/auth/verify/{token}"
+    try:
 
-    print("Verification link:", link)
+        link = f"https://kazi-mikononi-back-end.onrender.com/auth/verify/{token}"
+
+        msg = Message(
+            subject="Verify Your Kazi Mikononi Account",
+            sender=current_app.config["MAIL_DEFAULT_SENDER"],
+            recipients=[user_email]
+        )
+
+        msg.body = f"""
+        Verify your email:
+
+        {link}
+        """
+
+        msg.html = f"""
+        <h2>Verify Email</h2>
+
+        <a href="{link}">
+            Verify Account
+        </a>
+        """
+
+        mail.send(msg)
+
+        print("EMAIL SENT SUCCESSFULLY")
+
+    except Exception as e:
+
+        print("EMAIL FAILED")
+        print(str(e))
