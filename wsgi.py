5cac11f90b526032b4b79a502e47254573153a26
Jeremy Junior <jrjn71780@gmail.com>
2026-05-21 18:48:38 +0300

    removed ping

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,6 @@ from extensions import socketio
 
 app = create_app()
 
-@app.route("/ping")
-def ping():
-    return {"status": "alive"}
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
