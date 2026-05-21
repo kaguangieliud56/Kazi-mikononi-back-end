5d3ba001e25098e1f523bcb7d95e260153647c7f
Jeremy Junior <jrjn71780@gmail.com>
2026-05-22 00:35:54 +0300

    changes

--- extensions.py
+++ extensions.py
@@ -16,5 +16,6 @@ socketio = SocketIO(
     cors_allowed_origins="*",
     async_mode="threading",
     logger=True,
-    engineio_logger=True
+    engineio_logger=True,
+    transports=["polling"]  # 🔥 ADD THIS
 )
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
 from app import create_app
 from extensions import socketio
 
-
 app = create_app()
 
+socketio.init_app(app)
 
-if __name__ == "__main__":
-    socketio.run(app, debug=True)
+# ❌ DO NOT use socketio.run here
